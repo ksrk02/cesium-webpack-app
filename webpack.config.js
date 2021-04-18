b679bc7ae1d62b0be01d60f0697e9ebcab1c8043
ksrk02 <xgrk9475@gmail.com>
2021-04-18 12:54:05 +0900

    v1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopywebpackPlugin = require('copy-webpack-plugin');
+
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
+
+module.exports = {
+    mode: 'development',
+    context: __dirname,
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+
+        sourcePrefix: ''
+    },
+    amd: {
+        toUrlUndefined: true
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+            use: ['url-loader']
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CopywebpackPlugin({
+            patterns: [
+                { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
+                { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
+                { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
+            ]
+        }),
+        new webpack.DefinePlugin({
+            CESIUM_BASE_URL: JSON.stringify('')
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist")
+    },
+    resolve: {
+        alias: {
+            cesium: path.resolve(__dirname, cesiumSource)
+        }
+    },
+};
